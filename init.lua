98ee9d2f29cf64aa26887ef75dfe852615b35481
Jiashu Xu <jxu1@fas.harvard.edu>
2023-04-04 17:56:04 -0400

    add vs code config

--- init.lua
+++ init.lua
@@ -5,20 +5,25 @@
 --  ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛
 
 -- Import Lua modules
-require('core/lazy')
-require('core/autocmds')
-require('core/keymaps')
-require('core/colors')
-require('core/statusline')
-require('core/options')
-require('plugins/nvim-tree')
-require('plugins/indent-blankline')
-require('plugins/nvim-cmp')
-require('plugins/nvim-lspconfig')
-require('plugins/nvim-treesitter')
-require('plugins/alpha-nvim')
-require('plugins/dressing')
-require('plugins/nvim-navic')
-require('plugins/quickui')
-require('plugins/vimtex')
-require('plugins/vim_markdown')
+if vim.g.vscode then
+  print("vscode")
+  vim.opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
+else
+  require('core/lazy')
+  require('core/autocmds')
+  require('core/keymaps')
+  require('core/colors')
+  require('core/statusline')
+  require('core/options')
+  require('plugins/nvim-tree')
+  require('plugins/indent-blankline')
+  require('plugins/nvim-cmp')
+  require('plugins/nvim-lspconfig')
+  require('plugins/nvim-treesitter')
+  require('plugins/alpha-nvim')
+  require('plugins/dressing')
+  require('plugins/nvim-navic')
+  require('plugins/quickui')
+  require('plugins/vimtex')
+  require('plugins/vim_markdown')
+end
