534c44c25e4e1702cba0b53985795cc18c7ae8dc
Jiashu Xu <jxu1@fas.harvard.edu>
2023-04-15 02:05:11 -0400

    add

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -141,8 +141,22 @@ lazy.setup({
         -- Jump forwards/backwards with '{' and '}'
         vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
         vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
-      end
-    }
+      end,
+      layout = {
+        -- These control the width of the aerial window.
+        -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_width and max_width can be a list of mixed types.
+        -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+        -- max_width = { 100, 0.5 },
+        max_width = { 0.5 },
+        min_width = { 0.2 },
+        -- Determines the default direction to open the aerial window. The 'prefer'
+        -- options will open the window in the other direction *if* there is a
+        -- different buffer in the way of the preferred direction
+        -- Enum: prefer_right, prefer_left, right, left, float
+        default_direction = "prefer_right",
+      },
+    },
   },
 
   -- UndoTree
@@ -184,6 +198,7 @@ lazy.setup({
    },
    opts = {
      ignore = {
+       buftype = { 'quickfix' },
        filetype = {"qf"} -- tex compile error window is not maximized
      }
    },
